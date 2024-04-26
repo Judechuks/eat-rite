fd199897fa6b48923b11b61adf8903ed754bf8a2
Monye Jude <Judechuks26@gmail.com>
2024-04-26 08:48:01 +0000

    adjustment to navbar and click event for menu links

--- static/script.js
+++ static/script.js
@@ -1,7 +1,16 @@
 // members/static/script.js
 const navBar = document.querySelector(".navbar");
 const menuBar = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu a");
 
+// close menu when the navbar is clicked
 navBar.addEventListener('click', () => {
   menuBar.classList.toggle('active');
+})
+
+// close menu when each menuLink is clicked
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener('click', () => {
+    menuBar.classList.toggle('active');
+  })
 })
