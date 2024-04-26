927f4ea1a918975b4327cd4f69c36a562b2214d2
Monye Jude <Judechuks26@gmail.com>
2024-04-26 09:10:49 +0000

    newsapi integration and populated in the UI

--- django_project/views.py
+++ django_project/views.py
@@ -1,12 +1,23 @@
 import requests
 from django.shortcuts import render
 
+API_KEY = "YOUR_API_KEY"
+
 
 def home(request):
-  # using API
+  # meals API
   response = requests.get(
       'https://www.themealdb.com/api/json/v1/1/search.php?s=')
   data = response.json()
   meals = data['meals'][0:8]
 
-  return render(request, 'templates/index.html', {'meals': meals})
+  # news API
+  response = requests.get(
+      f'https://newsapi.org/v2/top-headlines?country=us&apikey={API_KEY}')
+  data = response.json()
+  newslist = data['articles']
+
+  return render(request, 'templates/index.html', {
+      'meals': meals,
+      'newslist': newslist
+  })
