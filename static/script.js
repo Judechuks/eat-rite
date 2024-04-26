27b35d4b87a2be134b86bdfb6669a9784b558aef
Monye Jude <Judechuks26@gmail.com>
2024-04-26 08:40:03 +0000

    styles and script for navigation bar

--- static/script.js
+++ static/script.js
@@ -1,1 +1,7 @@
 // members/static/script.js
+const navBar = document.querySelector(".navbar");
+const menuBar = document.querySelector(".menu");
+
+navBar.addEventListener('click', () => {
+  menuBar.classList.toggle('active');
+})
